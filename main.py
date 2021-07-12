4eacfa781b739928710b7c9a124271b14cc56a61
AFOEK <felix.montalfu@gmail.com>
2021-07-12 13:34:57 +0700

    minor fixing

--- main.py
+++ main.py
@@ -157,8 +157,8 @@ def stat(img=None):
             autogen.data_frames.plot(x="Number", y="Frequency", kind="bar", legend=True, ax=ax)
             ax.set_title("Frequency distribution among generated random number")
             figure.savefig(output, format="png")
-            data = base64.b64encode(output.getbuffer()).decode("ascii")
             output.seek(0)
+            data = base64.b64encode(output.getbuffer()).decode("ascii")
             return render_template("stat.html", img=data)
         elif opt == "scatter":
             figure = Figure(figsize=(6,6), dpi=110)
@@ -167,16 +167,16 @@ def stat(img=None):
             autogen.data_frames.plot(x="Number", y="Frequency", kind="scatter", legend=True, ax=ax)
             ax.set_title("Frequency distribution among generated random number")
             figure.savefig(output, format="png")
-            data = base64.b64encode(output.getbuffer()).decode("ascii")
             output.seek(0)
+            data = base64.b64encode(output.getbuffer()).decode("ascii")
             return render_template("stat.html", img=data)
         elif opt == "heatmap":
             figure, ax = plt.subplots(figsize=(6,6), dpi=110)
             sns.heatmap(autogen.data_frames, cmap='YlGnBu', annot=True)
             output = io.BytesIO()
             figure.savefig(output, format="png")
-            data = base64.b64encode(output.getbuffer()).decode("ascii")
             output.seek(0)
+            data = base64.b64encode(output.getbuffer()).decode("ascii")
             return render_template("stat.html", img=data)
     return temp_string
 
